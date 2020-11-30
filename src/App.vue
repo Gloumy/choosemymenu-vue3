6e50ea6100e04e10385043d67f40ec2d4920156f
Simon <simon@cornet.dev>
2020-11-30 10:28:43 +0100

    use julien's design -- needs refacto/cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,68 @@
 <template>
-  <h1>Hello world !</h1>
+  <div>
+    <header>
+      <div class="grid-container grid-container-padded">
+        <ul class="grid-x grid-margin-x">
+          <li class="small-4 cell">
+            <h1>
+              <a class="no_css h1_hidden" href="#body"
+                >Choose my menu <img src="./assets/img/logo.png" alt="Logo"
+              /></a>
+            </h1>
+          </li>
+          <li class="small-8 cell intro_app">
+            <p>
+              Laisse l'application te proposer des plats originaux et verrouille
+              chaque jour qui te plait. Ensuite, partage à tes proches, ces
+              délicieuses recettes !
+            </p>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main id="main">
+      <section>
+        <div class="grid-container grid-container-padded">
+          <div class="grid-x grid-margin-x">
+            <div class="cell small-12">
+              <div
+                class="small-12 cell list_item"
+                v-for="index in 7"
+                :key="index"
+              >
+                <div class="grid-x grid-margin-x align-middle">
+                  <div class="cell small-2">
+                    <img src="./assets/img/cadenas-open.svg" alt="cadenas" />
+                  </div>
+                  <div class="cell small-3 jour">MARDI</div>
+                  <div class="cell small-5 titre_recette">
+                    Oeufs à la tomate et chorizo
+                  </div>
+                  <div
+                    class="cell small-2 photo_recette"
+                    style="background-image: url(./assets/img/pizza.jpg)"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="mobile-app-icon-bar">
+        <button><img src="./assets/img/picto-list.svg" alt="Picto" /></button>
+        <button>
+          <img
+            class="choose"
+            src="./assets/img/picto-melange.svg"
+            alt="Picto"
+          />
+        </button>
+        <button class="active">
+          <img src="./assets/img/picto-share.svg" alt="Picto" />
+        </button>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,4 +75,5 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "@/assets/scss/app";
 </style>
